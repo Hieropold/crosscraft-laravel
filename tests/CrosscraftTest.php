afe08fd995ec47f2a72da3fe30066441ca27779e
Ilya Kurnosenkov <ilya.kurnosenkov@nantmobile.com>
2015-10-02 22:39:02 +0300

    Added some basic tests.

--- tests/CrosscraftTest.php
+++ tests/CrosscraftTest.php
@@ -4,16 +4,14 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class CrosscraftTest extends TestCase
 {
     /**
-     * A basic functional test example.
-     *
      * @return void
      */
-    public function testBasicExample()
+    public function testLandingPage()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('Crosscraft');
     }
 }
